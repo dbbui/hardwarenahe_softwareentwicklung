8f6f34c4e3c859b8b39c289594284c5eee5b42d2
dbbui <ic25b001@technikum-wien.at>
2025-09-14 18:02:26 +0200

    Add files via upload

---
+++ matrikel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[] = "Dumitru Buiucli";
+    char matrikel[] = "ic25b001"; 
+    char gruppe[] = "1A1";
+    
+    printf("%s,\n", name);
+    printf("%s,\n", matrikel);
+    printf("%s\n", gruppe);      
+    
+    return 0;
+}
